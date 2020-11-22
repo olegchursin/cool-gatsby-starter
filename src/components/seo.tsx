e693a7d5abe4409bb026959903381fe3c223142a
olegchursin <olegchursin@gmail.com>
2020-11-22 01:28:08 -0500

    fix:  update SEO component

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -2,71 +2,133 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
+interface MetaItem {
+  readonly name: string;
+  readonly content: string;
+}
 interface ISEOProps {
+  readonly title?: string;
   readonly description?: string;
   readonly lang?: string;
-  readonly meta?: [];
-  readonly title: string;
+  readonly meta?: MetaItem[];
+  readonly url?: string;
+  readonly author?: string;
+  readonly keywords?: string[];
+  readonly image?: string;
 }
 
-const SEO: React.FC<ISEOProps> = ({ description, lang, meta, title }) => {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
+const SEO: React.FC<ISEOProps> = (props) => {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+          author
+          url
+          keywords
+          image
         }
       }
-    `
-  );
+    }
+  `);
 
-  const metaDescription = description || site.siteMetadata.description;
+  const { siteMetadata } = data.site;
+  const {
+    title,
+    description,
+    url,
+    author,
+    meta = [],
+    keywords = [],
+    image
+  } = siteMetadata;
+
+  const siteTitle = title;
+  const pageTitle = props.title || '';
+  const siteDescription = props.description || description;
+  const siteUrl = props.url || url;
+  const siteLang = props.lang || 'en';
+  const siteAuthor = props.author || author;
+  const siteImage = props.image || image;
+  const siteKeywords = [...keywords, props.keywords].join(',');
+  const metaData = [
+    {
+      name: 'canonical',
+      content: siteUrl
+    },
+    {
+      name: 'description',
+      content: siteDescription
+    },
+    {
+      name: 'image',
+      content: siteImage
+    },
+    {
+      name: 'og:url',
+      content: siteUrl
+    },
+    {
+      name: 'og:type',
+      content: 'article'
+    },
+    {
+      name: 'og:title',
+      content: siteTitle
+    },
+    {
+      name: 'og:description',
+      content: siteDescription
+    },
+    {
+      name: 'og:image',
+      content: siteImage
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image'
+    },
+    {
+      name: 'twitter:creator',
+      content: siteAuthor
+    },
+    {
+      name: 'twitter:title',
+      content: siteTitle
+    },
+    {
+      name: 'twitter:description',
+      content: siteDescription
+    },
+    {
+      name: 'twitter:image',
+      content: siteImage
+    },
+    {
+      name: 'keywords',
+      content: siteKeywords
+    }
+  ].concat(meta);
+
+  const linkData = [
+    {
+      rel: 'shortcut icon',
+      href: 'favicon.ico'
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: 'icons/apple-touch-icon.png'
+    }
+  ];
 
   return (
     <Helmet
-      htmlAttributes={{
-        lang
-      }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription
-        },
-        {
-          property: `og:title`,
-          content: title
-        },
-        {
-          property: `og:description`,
-          content: metaDescription
-        },
-        {
-          property: `og:type`,
-          content: `website`
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author
-        },
-        {
-          name: `twitter:title`,
-          content: title
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription
-        }
-      ].concat(meta || [])}
+      htmlAttributes={{ lang: siteLang }}
+      title={siteTitle}
+      titleTemplate={`${siteTitle} | ${pageTitle}`}
+      meta={metaData}
+      link={linkData}
     />
   );
 };
